4db6470d597d55bb87a9fffb94401f606f114583
Ji S <j2yshin@gmail.com>
2018-10-25 17:08:57 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,6 +4,7 @@ def alphabetize(arr)
   esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
   arr.sort_by do |phrase|
     phrase.chars.collect do |char|
+      binding.pry
       esperanto.index(char)
     end
   end
